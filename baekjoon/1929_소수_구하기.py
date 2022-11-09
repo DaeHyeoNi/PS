64c43cf23e4d393455ace149bf6c5f2bfbf26465
daehyeoni <daehyeoni3333@gmail.com>
2022-11-10 00:16:14 +0900

    ✅ Solve Baekjoon 1929

---
+++ baekjoon/1929_소수_구하기.py
@@ -0,0 +1,12 @@
+def isPrimeNumber(num):
+    last = int(num**(1/2))
+    for i in range(2, last + 1):
+        if num % i == 0:
+            return False
+
+    return True
+
+m, n = map(int, input().split())
+for i in range(m, n + 1):
+    if isPrimeNumber(i):
+        print(i)
