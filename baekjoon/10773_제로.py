ffcd72753789bcf5a75fc174219e537b9efd62aa
daehyeoni <daehyeoni3333@gmail.com>
2022-11-25 23:43:13 +0900

    ✅ Solve Baekjoon 10773

---
+++ baekjoon/10773_제로.py
@@ -0,0 +1,9 @@
+K = int(input())
+numbers = []
+for _ in range(K):
+    number = int(input())
+    if number == 0:
+        numbers.pop()
+    else:
+        numbers.append(number)
+print(sum(numbers))
