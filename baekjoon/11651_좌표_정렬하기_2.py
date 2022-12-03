59df93b2b21c22e95498024391fb832a5b9d6c27
daehyeoni <daehyeoni3333@gmail.com>
2022-12-03 17:38:35 +0900

    ✅ Solve Baekjoon 11651

---
+++ baekjoon/11651_좌표_정렬하기_2.py
@@ -0,0 +1,5 @@
+# fmt: off
+import sys
+input, N, coords = sys.stdin.readline, int(input()), []
+[coords.append(list(map(int, input().split()))) for _ in range(N)]
+[print(f'{coord[0]} {coord[1]}') for coord in sorted(coords, key=lambda x: (x[1], x[0]))]
