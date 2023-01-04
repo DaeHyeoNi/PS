df10ff527e4a7187b20610532e2b22082221104c
daehyeoni <daehyeoni3333@gmail.com>
2023-01-04 12:28:55 +0900

    ✅ Solve Baekjoon 2775

---
+++ baekjoon/2775_부녀회장이_될테야.py
@@ -0,0 +1,10 @@
+test_case = int(input())
+
+for _ in range(test_case):
+    k = int(input())
+    n = int(input())
+    people = [i for i in range(1, n + 1)]
+    for _ in range(k):
+        for j in range(1, n):
+            people[j] += people[j - 1]
+    print(people[-1])
